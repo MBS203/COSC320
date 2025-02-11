f9db2ff7309d8383b55ef6feb50cd97bb191c848
MBS203 <malshebli@hotmail.com>
2025-02-11 09:44:58 +0400

    Add files via upload

---
+++ 03_if/if2.rs
@@ -0,0 +1,42 @@
+fn picky_eater(food: &str) -> &str {
+    if food == "strawberry" {
+        "Yummy!"
+    } else if food == "potato" {
+        "I guess I can eat that."
+    } else {
+        "No thanks!"
+    }
+}
+
+fn main() {
+    let food = "strawberry";
+    println!("If you give me {}, I say: {}", food, picky_eater(food));
+
+    let food = "potato";
+    println!("If you give me {}, I say: {}", food, picky_eater(food));
+
+    let food = "broccoli";
+    println!("If you give me {}, I say: {}", food, picky_eater(food));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn yummy_food() {
+        assert_eq!(picky_eater("strawberry"), "Yummy!");
+    }
+
+    #[test]
+    fn neutral_food() {
+        assert_eq!(picky_eater("potato"), "I guess I can eat that.");
+    }
+
+    #[test]
+    fn default_disliked_food() {
+        assert_eq!(picky_eater("broccoli"), "No thanks!");
+        assert_eq!(picky_eater("gummy bears"), "No thanks!");
+        assert_eq!(picky_eater("literally anything"), "No thanks!");
+    }
+}
